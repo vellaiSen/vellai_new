f6e3ade29c0b550104105bd99c1361d17c39c2f1
vellaiSen <vellai.mek@gmail.com>
2021-10-30 23:48:47 +0800

    welcome

---
+++ Backery.java
@@ -0,0 +1,19 @@
+public class Backery {
+	static  String Backery_name = "vellai";
+	int doornum = 4;
+	int price;
+	public static void main(String[] arg) {
+	Backery cake = new Backery();
+		cake.price = 10;
+	Backery cooldrinks = new Backery();
+		cooldrinks.price = 20;
+	Backery vadai = new Backery();
+		vadai.price = 30;
+	Backery bonda = new Backery();
+		System.out.println(Backery_name);
+		System.out.println(cake.price);
+		System.out.println(cooldrinks.price);
+		System.out.println(vadai.price);
+	}
+}
+
