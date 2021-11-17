0f4eaaa4fa52e4bfa255f76357457cfcda443a7e
vellaiSen <vellai.mek@gmail.com>
2021-11-17 20:56:16 +0800

    inherit program

---
+++ Mobile.java
@@ -0,0 +1,14 @@
+package nextgeneration;
+
+import nineties.Phone;
+
+public class Mobile extends Phone
+{
+public static void main(String[] args)
+{
+Mobile vivo = new Mobile();
+vivo.call();
+vivo.receive();
+vivo.fixprice();
+}
+}
