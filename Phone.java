5d08b06059a14cae4f034bc0de928f64f24f3fc7
vellaiSen <vellai.mek@gmail.com>
2021-11-17 20:54:42 +0800

    inheritance example

---
+++ Phone.java
@@ -0,0 +1,21 @@
+package nineties;
+public class Phone
+{
+	private int price = 600;
+	
+public static void main(String[] args)
+{
+
+private void fixprice()
+{
+System.out.println("1000");	
+}
+public void call()
+{
+System.out.println("outgoing");
+}
+public void receive()
+{
+System.out.println("receiving");
+}
+}
