78f23ab7c30ba31c261778d155629ed813d80a28
vellaiSen <vellai.mek@gmail.com>
2021-11-20 21:14:22 +0800

    protected class accessing using extends

---
+++ Daughter.java
@@ -0,0 +1,13 @@
+package kovai;
+import nativeplace.GrandMother;
+import chennai.Mom;
+
+public class Daughter extends Mom
+{
+
+public static void main(String[] args)
+{
+Daughter d = new Daughter();
+d.invite();
+}
+}
