e93a8699c9c38a281560cbc0b0d725120e318846
vellaiSen <vellai.mek@gmail.com>
2021-11-20 21:10:03 +0800

    protected method

---
+++ GrandMother.java
@@ -0,0 +1,15 @@
+package nativeplace;
+public class GrandMother
+{
+
+public static void main(String[] args)
+{
+
+GrandMother gr = new GrandMother();
+}
+protected void invite()
+{
+
+System.out.println("event");
+}
+}
